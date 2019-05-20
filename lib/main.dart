6c292e893eec6e68745b673b53d983d9a358ac03
Nexxera <nexxera@Nexxeras-Mac-mini.local>
2019-05-20 11:08:58 -0300

    Initial changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,13 @@ import 'package:flutter/services.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     _changeStatusBar();
     return new MaterialApp(
-      title: 'Mana Initial Prototype',
-      theme: new ThemeData(
-        primarySwatch: Colors.pink
-      ),
-      home: new LoginSignUpPage()
-    );
+        title: 'Mana Initial Prototype',
+        theme: new ThemeData(primarySwatch: Colors.pink),
+        home: new LoginSignUpPage());
   }
 
   void _changeStatusBar() {
--- lib/widgets/login_signup_widgets.dart
+++ lib/widgets/login_signup_widgets.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:login_fb/enum/login_signup_enum.dart';
 
 class LoginSignUpWidgets {
-
   static Widget showCircularProgress({@required bool isLoading}) {
     if (isLoading) {
       return Center(child: CircularProgressIndicator());
@@ -10,7 +9,7 @@ class LoginSignUpWidgets {
     return Container(height: 0.0, width: 0.0);
   }
 
-   static Widget showLogo() {
+  static Widget showLogo() {
     return new Hero(
       tag: 'hero',
       child: Padding(
@@ -27,16 +26,19 @@ class LoginSignUpWidgets {
   static Widget showEmailInput({@required String email}) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(16.0, 100.0, 16.0, 0.0),
-      child: new TextFormField(
+      child: TextFormField(
         maxLines: 1,
         keyboardType: TextInputType.emailAddress,
         autofocus: false,
         decoration: new InputDecoration(
-          hintText: 'Email',
-          icon: new Icon(
-            Icons.mail,
-            color: Colors.grey
-          )
+          // hintText: 'Email',
+          labelText: 'Email',
+          // border: InputBorder.none,
+          filled: true,
+          fillColor: Colors.grey[300],
+          // border: OutlineInputBorder(),
+          // focusedBorder: OutlineInputBorder(),
+          // icon: new Icon(Icons.mail, color: Colors.grey)
         ),
         validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
         onSaved: (value) => email = value,
@@ -46,17 +48,16 @@ class LoginSignUpWidgets {
 
   static Widget showPasswordInput({@required String password}) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 0.0),
       child: new TextFormField(
         maxLines: 1,
         obscureText: true,
         autofocus: false,
         decoration: new InputDecoration(
-          hintText: 'Password',
-          icon: new Icon(
-            Icons.lock,
-            color: Colors.grey
-          )
+          labelText: 'Password',
+          filled: true,
+          fillColor: Colors.grey[300],
+          // icon: new Icon(Icons.lock, color: Colors.grey)
         ),
         validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
         onSaved: (value) => password = value,
@@ -64,63 +65,83 @@ class LoginSignUpWidgets {
     );
   }
 
-  static Widget showPrimaryButton({
-     @required FormMode formMode, 
-     @required Function validateAndSubmit,
-     @required bool isLoading
-   }) {
+  static Widget showPrimaryButton(
+      {@required FormMode formMode,
+      @required Function validateAndSubmit,
+      @required bool isLoading}) {
     return Padding(
       padding: EdgeInsets.fromLTRB(24.0, 45.0, 24.0, 0.0),
-      child: MaterialButton(
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16.0) ), // Adding Border to any widget
-        minWidth: 180.0,
-        height: 32.0,
+      child: OutlineButton(
+        highlightedBorderColor: Colors.pink[300],
+        shape: RoundedRectangleBorder(
+            borderRadius:
+                BorderRadius.circular(16.0)), // Adding Border to any widget
+        // minWidth: 180.0,
+        // height: 32.0,
         color: Colors.pink,
-        child: isLoading ? _showLoading() : _primaryButtonLabel(formMode: formMode),
+        child: isLoading
+            ? _showLoading()
+            : _primaryButtonLabel(formMode: formMode),
         onPressed: validateAndSubmit,
       ),
     );
   }
 
+  // static Widget showPrimaryButton(
+  //     {@required FormMode formMode,
+  //     @required Function validateAndSubmit,
+  //     @required bool isLoading}) {
+  //   return Padding(
+  //     padding: EdgeInsets.fromLTRB(24.0, 45.0, 24.0, 0.0),
+  //     child: MaterialButton(
+  //       elevation: 5.0,
+  //       shape: RoundedRectangleBorder(
+  //           borderRadius:
+  //               BorderRadius.circular(16.0)), // Adding Border to any widget
+  //       minWidth: 180.0,
+  //       height: 32.0,
+  //       color: Colors.pink,
+  //       child: isLoading
+  //           ? _showLoading()
+  //           : _primaryButtonLabel(formMode: formMode),
+  //       onPressed: validateAndSubmit,
+  //     ),
+  //   );
+  // }
+
   static Widget _showLoading() {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 8.0),
-      child: Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white))),
-      );
+      child: Center(
+          child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white))),
+    );
   }
 
-  static Widget showSecondaryButton({
-    @required FormMode formMode, 
-    @required Function changeFormToSignUp, 
-    @required Function changeFormToLogin
-  }) {
+  static Widget showSecondaryButton(
+      {@required FormMode formMode,
+      @required Function changeFormToSignUp,
+      @required Function changeFormToLogin}) {
     return new Padding(
       padding: EdgeInsets.only(top: 48.0),
       child: FlatButton(
-      child: _secondaryButtonLabel(formMode: formMode),
-      onPressed: formMode == FormMode.LOGIN
-        ? changeFormToSignUp
-        : changeFormToLogin,
+        child: _secondaryButtonLabel(formMode: formMode),
+        onPressed:
+            formMode == FormMode.LOGIN ? changeFormToSignUp : changeFormToLogin,
       ),
     );
   }
 
   static Widget showErrorMessage({@required String errorMessage}) {
     if (errorMessage != null && errorMessage.length > 0) {
-      return new Text(
-        errorMessage,
-        style: TextStyle(
-          fontSize: 13.0,
-          color: Colors.red,
-          height: 1.0,
-          fontWeight: FontWeight.w300
-        )
-      );
+      return new Text(errorMessage,
+          style: TextStyle(
+              fontSize: 13.0,
+              color: Colors.red,
+              height: 1.0,
+              fontWeight: FontWeight.w300));
     } else {
-      return new Container(
-        height: 0.0
-      );
+      return new Container(height: 0.0);
     }
   }
 
@@ -130,13 +151,15 @@ class LoginSignUpWidgets {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 8.0),
       child: Text(textLabel, style: textStyle),
-      );
+    );
   }
 
   static Widget _secondaryButtonLabel({@required FormMode formMode}) {
-    TextStyle textStyle = new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300);
-    String textLabel = formMode == FormMode.LOGIN ? 'Create an Account' : 'Have an Account? Sign in';
+    TextStyle textStyle =
+        new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300);
+    String textLabel = formMode == FormMode.LOGIN
+        ? 'Create an Account'
+        : 'Have an Account? Sign in';
     return Text(textLabel, style: textStyle);
   }
-
 }
